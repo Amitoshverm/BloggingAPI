0f774e32d25afde8302dd6c126e9e450767131a8
Amitosh <amitosh.verma9@gmail.com>
2023-03-13 16:40:47 +0530

    JWT working fine
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/example/blogapi/security/jwt/JWTService.java
+++ src/main/java/com/example/blogapi/security/jwt/JWTService.java
@@ -1,2 +1,30 @@
-package com.example.blogapi.security.jwt;public class jwtService {
+package com.example.blogapi.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Date;
+
+@Service
+public class JWTService {
+
+
+    private Algorithm algorithm = Algorithm.HMAC256("SECRETE SIGNING KEY (should be environment or config");
+    public String createJWT(Integer userId){
+        String token = JWT.create()
+                .withSubject(userId.toString())
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) /* 7 days */
+                .sign(algorithm);
+        return token;
+    }
+
+//    public Integer getUserIdFromJWT(String JWT){
+//        var verifier = JWT.require(algorithm).build;
+//        var decodedJWT = verifier.verify(JWT);
+//        var subject = decodedJWT.getSubject();
+//        return Integer.parseInt(subject);
+//    }
 }
--- src/test/java/com/example/blogapi/security/jwt/JWTServiceTest.java
+++ src/test/java/com/example/blogapi/security/jwt/JWTServiceTest.java
@@ -1,2 +1,25 @@
-package com.example.blogapi.security.jwt;public class JWTServiceTest {
+package com.example.blogapi.security.jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class JWTServiceTest {
+
+    private JWTService jwtService = new JWTService();
+
+    @Test
+    void canCreateJWTFromUserID(){
+        var userId = 1112;
+        var jwt = jwtService.createJWT(userId);
+        assertEquals("aaa", jwt);
+    }
+
+//    @Test
+//    void canVerifyJWT() {
+//        var jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMTEyIn0.OXtVbtIYFMSiIkraopI7Pu5iWJRNPyBX1Oa7kFffAKc";
+//        var userId = jwtService.getUser
+//    }
+
 }
